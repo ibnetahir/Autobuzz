96a0d261602906eda1c0d4243d51462bbfdeca03
ibnetahir <khaleeq419@gmail.com>
2017-06-30 13:08:34 -0800

    autobuzz

---
+++ AutoBuzz/Controllers/ShopController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using AutoBuzz.Models;
+
+namespace AutoBuzz.Controllers
+{
+   
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult products_grid()
+        {
+            return View();
+        }
+        public ActionResult products_list()
+        {
+            return View();
+        }
+        public ActionResult cart()
+        {
+            return View();
+        }
+        public ActionResult single_product()
+        {
+            return View();
+        }
+    }
+}
---
+++ AutoBuzz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoBuzz.Startup))]
+namespace AutoBuzz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
